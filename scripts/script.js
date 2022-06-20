2b2d32b9554a3b79a5d1950daf1d78e68eab1d84
salmabader <salma-bader-15@hotmail.com>
2022-06-20 10:09:51 +0300

    show and hide menus

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+function showOrHideMenu1() {
+    let menu = document.getElementById("submenu1");
+    let icon = document.getElementById("collectionIcon");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+        icon.className = "bi-collection";
+    }
+    else {
+        menu.style.display = "none";
+        icon.className = "bi-collection-fill";
+    }
+}
+function showOrHideMenu2() {
+    let menu = document.getElementById("submenu2");
+    let icon = document.getElementById("collectionIcon2");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+        icon.className = "bi-collection";
+    }
+    else {
+        menu.style.display = "none";
+        icon.className = "bi-collection-fill";
+    }
+}
