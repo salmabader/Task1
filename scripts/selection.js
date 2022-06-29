3dc107f3b0281e8c774c5c7a35df956c428d553c
salmabader <salma-bader-15@hotmail.com>
2022-06-29 11:10:57 +0300

    all done

--- scripts/selection.js
+++ scripts/selection.js
@@ -1,5 +1,5 @@
 let isComplete = false;
-
+let submissionTimes = 0;
 // -------- handle cards --------
 let index1 = 0;
 let index2 = 0;
@@ -111,16 +111,93 @@ function next(id, direction, index, nextSlide, prevSlide, slides) {
 }
 
 // -------- handle submission button --------
+let allCards = document.querySelectorAll('.card');
 function checkAnswers() {
-    let correctAnswers = [5, 3, 1, 2, 4];
+    submissionTimes++;
     if (index1 == 5 && index2 == 3 && index3 == 1 && index4 == 2 && index5 == 4) {
-        let allCards = document.querySelectorAll('.card');
         for (let i = 0; i < allCards.length; i++) {
             allCards[i].classList.add('correct');
         }
         document.getElementById('btn1').disabled = true;
         document.getElementById('btn2').disabled = true;
         document.getElementById('btn3').disabled = true;
+        return;
     }
+    if (index1 != 5) document.querySelector('.firstContent').classList.add('wrong');
+    else document.querySelector('.firstContent').classList.add('correct');
+    if (index2 != 3) document.querySelector('.secondContent').classList.add('wrong');
+    else document.querySelector('.secondContent').classList.add('correct');
+    if (index3 != 1) document.querySelector('.thirdContent').classList.add('wrong');
+    else document.querySelector('.thirdContent').classList.add('correct');
+    if (index4 != 2) document.querySelector('.fourthContent').classList.add('wrong');
+    else document.querySelector('.fourthContent').classList.add('correct');
+    if (index5 != 4) document.querySelector('.fifthContent').classList.add('wrong');
+    else document.querySelector('.fifthContent').classList.add('correct');
+    document.getElementById('btn1').disabled = true;
+    if (submissionTimes < 2) {
+        document.getElementById('btn2').disabled = false;
+    }
+    else {
+        document.getElementById('btn3').disabled = false;
+    }
+    hideArrows();
+    $('html, body').animate({ scrollTop: '0px' }, 500);
+}
 
+// -------- handle try again button --------
+function tryAgain() {
+    showArrows();
+    let elements = document.querySelectorAll('.show');
+    for (el of elements) {
+        el.classList.remove('show');
+    }
+    for (card of allCards) {
+        card.classList.remove('correct');
+        card.classList.remove('wrong');
+    }
+    let defaultImg = document.querySelectorAll('#default');
+    for (de of defaultImg) {
+        de.classList.add('show');
+    }
+    document.getElementById('btn2').disabled = true;
+    $('html, body').animate({ scrollTop: '0px' }, 500);
+}
+
+// -------- handle solution button --------
+function showSolution() {
+    hideArrows();
+    $('html, body').animate({ scrollTop: '0px' }, 500);
+    let elements = document.querySelectorAll('.show');
+    for (el of elements) {
+        el.classList.remove('show');
+    }
+    for (card of allCards) {
+        card.classList.remove('correct');
+        card.classList.remove('wrong');
+    }
+    document.querySelector('.firstContent').children[5].classList.add('show');
+    document.querySelector('.secondContent').children[3].classList.add('show');
+    document.querySelector('.thirdContent').children[1].classList.add('show');
+    document.querySelector('.fourthContent').children[4].classList.add('show');
+    document.querySelector('.fifthContent').children[2].classList.add('show');
+    for (i of allCards) {
+        i.classList.add('correct');
+    }
+}
+
+function hideArrows() {
+    for (let i = 1; i <= 5; i++) {
+        let id = "left" + i;
+        document.getElementById(id).style.visibility = 'hidden';
+        id = "right" + i;
+        document.getElementById(id).style.visibility = 'hidden';
+    }
+}
+function showArrows() {
+    for (let i = 1; i <= 5; i++) {
+        let id = "left" + i;
+        document.getElementById(id).style.visibility = 'visible';
+        id = "right" + i;
+        document.getElementById(id).style.visibility = 'visible';
+    }
 }
